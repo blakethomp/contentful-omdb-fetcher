9963c3e5c570e175b03ebf359a62bcff210fc51b
Glitch (contentful-unsplash-app) <none>
2019-10-01 12:49:56 +0000

    🍼💕 Checkpoint ./package.json:7298813/222 ./webpack.config.js:7298813/186 ./app.js:7298813/1079

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import { init, locations } from 'contentful-ui-extensions-sdk';
 
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
 
-
-import { Typography, Button, Illustration } from '@contentful/forma-36-react-components';
+import { Note } from '@contentful/forma-36-react-components';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<Typography>
-    <Button buttonType="primary" onClick={() => alert('test')}>Hello world!</Button>
-    <Illustration illustration="Archive" />
-    </Typography>, root);
+
+init(sdk => {
+  if (sdk.location.is(locations.LOCATION_AP)) {
+    render(<AppConfig sdk={sdk} />, root);
+  } else if (sdk.location.is(locations.LOCATION_ENTRY_SIDEBAR)) {
+    render(<AppSidebar sdk={sdk} />, root);
+  }
+})
+
+class AppConfig extends React.Component {
+  
+  async componentDidMount () {
+    const parameters = await this.sdk.platformAlpha.app.getParameters();
+    
+    this.setState({ parameters })
+  }
+  
+  onConfigure () {
+    
+  }
+  
+}
+
+class AppSidebar extends React.Component {
+  
+}
--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        loader: ["style-loader", "css-loader"]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        loader: ["style-loader", "css-loader"]
       }
     ]
   }
