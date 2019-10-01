a0bf5b53eede2b8e3714e29a7bf66247ce220d28
Glitch (contentful-unsplash-app) <none>
2019-10-01 15:08:56 +0000

    🍏💍 Checkpoint ./app.js:7298813/996

--- app.js
+++ app.js
@@ -2,53 +2,41 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { init, locations } from 'contentful-ui-extensions-sdk';
-
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
-
-import { Typography, Heading, Note, Form, SelectField, Option } from '@contentful/forma-36-react-components';
+import { Spinner, Heading, Note, Form, SelectField, Option } from '@contentful/forma-36-react-components';
 
 const DEFAULT_ANIMAL = 'cat';
 
 init(sdk => {
-  const Component = sdk.location.is(locations.LOCATION_APP) ? AppConfig : AppSidebar;
+  const Component = sdk.location.is(locations.LOCATION_APP) ? Config : AnimalPicture;
   render(<Component sdk={sdk} />, document.getElementById('root'));
 });
 
-class AppConfig extends React.Component {
+class Config extends React.Component {
   constructor (props) {
     super(props);
-    this.state = { parameters: {}, ready: false };
+    this.state = { parameters: {} };
+    this.app = this.props.sdk.platformAlpha.app;
   }
-
+  
   async componentDidMount () {
-    const { app } = this.props.sdk.platformAlpha;
-
-    app.onConfigure(this.onConfigure.bind(this));
-    
-    this.setState({
-      parameters: await app.getParameters(),
-      ready: true
-    });    
+    this.app.onConfigure(() => this.onConfigure());
+    this.setState({ parameters: await this.app.getParameters() });
   }
   
   render () {
-    if (!this.state.ready) {
-      return null
-    }
-    
     return (
       <Form>
         <Heading>Daily Animal app</Heading>
         <Note noteType="primary" title="About the app">
-          Make life of your editors a little bit better with a cute animal picture in the entry editor sidebar.
+          Make editors in this space a little bit happier with a cute animal picture in the entry editor sidebar.
         </Note>
         <SelectField
           required
           name="animal-selection"
           id="animal-selection"
           labelText="Animal"
-          helpText="Pick the best kind of animal!"
           value={this.state.parameters.animal || DEFAULT_ANIMAL}
           onChange={e => this.setState({ parameters: { animal: e.target.value } })}
         >
@@ -60,9 +48,10 @@ class AppConfig extends React.Component {
     );
   }
   
-  onConfigure () {
-    const contentTypes = this.props.sdk.space.getContentTypes()
-    console.log(contentTypes)
+  async onConfigure () {
+    const { items: contentTypes } = await this.props.sdk.space.getContentTypes()
+    const contentTypeIds = contentTypes.map(ct => ct.sys.id)
+
     return {
       parameters: this.state.parameters,
       targetState: {
@@ -72,7 +61,7 @@ class AppConfig extends React.Component {
   }
 }
 
-function AppSidebar ({ sdk }) {
+function AnimalPicture ({ sdk }) {
   const animal = sdk.parameters.installation.animal || DEFAULT_ANIMAL;
         
   return <img src={`https://source.unsplash.com/300/300/?${animal}`} />
